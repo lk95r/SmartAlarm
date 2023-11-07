2260720cf31df3436575182c95bf1bf119265768
LKA <lennard.karger95@gmail.com>
2023-11-07 19:57:10 +0100

    adds user input from serial port for debugging

--- SmartAlarm.cpp
+++ SmartAlarm.cpp
@@ -8,7 +8,7 @@
 #include "hardware/clocks.h"
 #include "hardware/rtc.h"
 #include "pico/util/datetime.h"
-
+#include "libs/sm_display/sm_display.h"
 
 #include "pico/util/queue.h"
 #include "pico/multicore.h"
@@ -20,6 +20,9 @@
 extern queue_t q_date_time;
 datetime_t dt;
 bool error;
+SM_Display fsm_display;
+
+displayDirection e_menu_instruction;
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
@@ -29,14 +32,33 @@ bool error;
 #define PIN_SCK  18
 #define PIN_MOSI 19
 
-
+displayDirection getDirection(void){
+    char c_user_input;
+    displayDirection temp_insutruction;
+    c_user_input = getchar();
+    printf("Input: %c\n",c_user_input);
+    if(c_user_input == 'l'|| c_user_input == 'L'){
+        temp_insutruction = dLeft;
+    }else if(c_user_input == 'r'|| c_user_input == 'R'){
+        temp_insutruction = dRight;
+    }else if(c_user_input == 'u'|| c_user_input == 'U'){
+        temp_insutruction =dUp;
+    }else if(c_user_input = 'd'|| c_user_input =='D'){
+        temp_insutruction = dDown;
+    }else if(c_user_input = 'e'|| c_user_input == 'E'){
+        temp_insutruction = dEnter;
+    }else{
+        temp_insutruction = dNone;
+    }
+    return temp_insutruction;
+}
 
 void core1_entry() {
     core_1_main();
 }
 void check_rtc(){
     if(rtc_running()){
-        printf("c0: rtc läuft\n");
+        printf("c0: rtc running\n");
         rtc_get_datetime(&dt);
         printf("%d:%d:%d\n",dt.hour,dt.min,dt.sec);
         return;
@@ -58,7 +80,6 @@ void check_rtc(){
 #define TEST_NUM 10
 int main()
 {
-
     stdio_init_all();
     printf("c0: started\n");
     // SPI initialisation. This example will use SPI at 1MHz.
@@ -79,6 +100,8 @@ int main()
     while(1){
         printf("c0: alive\n");
         sleep_ms(1000);
+        e_menu_instruction = getDirection();
+        fsm_display.run(e_menu_instruction);
         check_rtc();
     }
     return 0;
